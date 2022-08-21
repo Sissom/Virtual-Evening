7c6cb5374ac205a19c342348677a91b2fd075adc
Sissom <111325589+Sissom@users.noreply.github.com>
2022-08-21 14:46:23 -0500

    Update exercise1.js

--- class2/exercise1/exercise1.js
+++ class2/exercise1/exercise1.js
@@ -30,8 +30,8 @@ const interests = ["Star Trek TNG", "LOTR The Two Towers", "Neuromancer", "pizza
 ];
 const favoriteMovie = interests[1]
 //console.log(favoriteMovie)
-
-// no default value declaration
 for (let favoriteBook of interests) {
     console.log(interests[2])
 }
+// no default value declaration
+const noValuePlaceholder
