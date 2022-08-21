0587ce9eca2a7966c7327440dd984ccea6fb644b
Sissom <111325589+Sissom@users.noreply.github.com>
2022-08-21 14:47:56 -0500

    Update exercise1.js

--- class2/exercise1/exercise1.js
+++ class2/exercise1/exercise1.js
@@ -1,38 +1,28 @@
 // string
 const username = "Sissom"
-//console.log(username)
 
 // number
 const age = 39;
-//console.log(age)
 
 // boolean
 const verificaton = (age === 39);
-//console.log(Boolean(age > 21))
 
 // null
 const hobby=null
-//console.log(hobby)
 
 // undefined
 const certificationStatus=undefined
-//console.log(certificationStatus)
 
 // object
 const pet = {animal: "dog",
 name: "Dante"
 }
 const petName = pet.name
-//console.log(petName)
 
 // array
 const interests = ["Star Trek TNG", "LOTR The Two Towers", "Neuromancer", "pizza", "MTN Dew Zero", "Skittles"
 ];
 const favoriteMovie = interests[1]
-//console.log(favoriteMovie)
-// for (let favoriteBook of interests) {
-//     console.log(interests[2])
-// }
 
 // no default value declaration
 const noValuePlaceholder
