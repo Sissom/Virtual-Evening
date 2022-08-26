0b367f218c64fb4102c4c87c227d93f3e991d1d3
Sissom <sissom@gmail.com>
2022-08-25 20:51:06 -0500

    update folders

--- class4/exercise1/exercise1.js
+++ class4/exercise1/exercise1.js
@@ -1,6 +1,12 @@
-function changeBackground() {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    document.body.style.backgroundColor = `#${randomColor}`;}
+const button = document.querySelector(".btn_onclick")
+
+function changeColors() {
+    const randomBackgroundColor = Math.floor(Math.random() * 16777215).toString(16);
+    const randomTextColor = Math.floor(Math.random() * 16777215).toString(16);
+
+    document.body.style.backgroundColor = `#${randomBackgroundColor}`;
+    document.body.style.color = `#${randomTextColor}`
+}
 
     const buttonEvent = document.querySelector(".btn_onclick");
-    buttonEvent.addEventListener("click", changeBackground);
+    buttonEvent.addEventListener("click", changeColors);
